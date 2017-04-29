0dd28b06bc1338a7447cd3e0c06f413f8f1dbe37
Alberto Bueno <abettetto@gmail.com>
2017-04-29 07:25:04 -0500

    Index
    Mejoras a Pagina Inicial

--- index.php
+++ index.php
@@ -1,11 +1,64 @@
 <html>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">
+        <img alt="SATNet" src="/">
+      </a>
+    </div>
+  </div>
+</nav>
 	<head>
 		<title>Inicio</title>
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-		<?php include "php/navbar.php"; ?>
 	</head>
 	<body>
+		<div class="col">
+		  <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="img/repuestos.jpg" alt="...">
+		      <div class="caption">
+		        <h3></h3>
+		        <p></p>
+		        <p>
+		        <a href="crearacta.php" class="btn btn-default" role="button">Entrega de Repuestos</a>
+		        </p>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+
+		<div class="col">
+		  <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="img/salidas.jpg" alt="...">
+		      <div class="caption">
+		        <h3></h3>
+		        <p></p>
+		        <p>
+		        <a href="crearacta.php" class="btn btn-default" role="button">Salida de Repuestos</a>
+		        </p>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+
+		<div class="col">
+		  <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="img/memorias.jpg" alt="...">
+		      <div class="caption">
+		        <h3></h3>
+		        <p></p>
+		        <p>
+		        <a href="crearacta.php" class="btn btn-default" role="button">Inventarios de Memorias</a>
+		        </p>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+
 
 	</body>
 </html>
