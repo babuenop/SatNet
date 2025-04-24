917966302a72875d5025e2d673d1969535b4c8ff
Alberto <abuenop@hotmail.com>
2025-04-23 21:29:37 -0500

    🛡️ Crear automáticamente grupos base ('tecnico', 'supervisor', 'seguridad', 'almacen') usando post_migrate

--- inventario/apps.py
+++ inventario/apps.py
@@ -1,9 +1,8 @@
 from django.apps import AppConfig
 
-
 class InventarioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'inventario'
 
     def ready(self):
-        import inventario.signals
+        import inventario.signals  # <-- Esto es lo que activa los grupos al migrar
